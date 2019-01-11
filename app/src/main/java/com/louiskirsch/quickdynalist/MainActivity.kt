6f27b690132bdba11637a05e0447edf0d5de30bb
Louis Kirsch <louis@louiskirsch.com>
2019-01-11 14:18:46 +0100

    Add info toast to location spinner and update version to 1.6

--- app/src/main/java/com/louiskirsch/quickdynalist/MainActivity.kt
+++ app/src/main/java/com/louiskirsch/quickdynalist/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.louiskirsch.quickdynalist
 
 import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.text.InputType
 import android.text.TextWatcher
+import android.view.Gravity
+import android.view.MotionEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.louiskirsch.quickdynalist.jobs.Bookmark
 import com.louiskirsch.quickdynalist.jobs.BookmarksJob
 import kotlinx.android.synthetic.main.activity_main.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
-import org.jetbrains.anko.contentView
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
 import java.util.*
 
 class MainActivity : Activity() {
     private val dynalist: Dynalist = Dynalist(this)
     private lateinit var adapter: ArrayAdapter<Bookmark>
+
+    private val preferences: SharedPreferences
+        get() = getSharedPreferences("MAIN_ACTIVITY", Context.MODE_PRIVATE)
+
+    private var bookmarkHintCounter: Int
+        get() = preferences.getInt("BOOKMARK_HINT_COUNTER", 0)
+        set(value) = preferences.edit().putInt("BOOKMARK_HINT_COUNTER", value).apply()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +44,15 @@ class MainActivity : Activity() {
             itemContents!!.text.clear()
         }
 
+        itemLocation.setOnTouchListener { _, e ->
+            if ((e.action == MotionEvent.ACTION_DOWN ||
+                    e.action == MotionEvent.ACTION_POINTER_DOWN) && bookmarkHintCounter < 5) {
+                longToast(R.string.bookmark_hint)
+                bookmarkHintCounter += 1
+            }
+            false
+        }
+
         val bookmarks = arrayOf(Bookmark.newInbox()) + dynalist.bookmarks
         adapter = ArrayAdapter(this,
                 android.R.layout.simple_spinner_item, bookmarks.toMutableList())
@@ -40,7 +60,7 @@ class MainActivity : Activity() {
         itemLocation!!.adapter = adapter
 
         val bookmarksOutdated = dynalist.lastBookmarkQuery.time <
-                Date().time - 60 * 60 * 1000L
+                Date().time - 60 * 1000L
         if (savedInstanceState == null && dynalist.isAuthenticated && bookmarksOutdated) {
             val jobManager = DynalistApp.instance.jobManager
             jobManager.addJobInBackground(BookmarksJob())
