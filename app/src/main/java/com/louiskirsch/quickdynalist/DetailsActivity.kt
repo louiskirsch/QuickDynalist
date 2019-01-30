3923aa1f7a647415c56de28466ea6292a0f413fd
Louis Kirsch <louis@louiskirsch.com>
2019-01-30 18:59:00 +0100

    Open image in gallery

--- app/src/main/java/com/louiskirsch/quickdynalist/DetailsActivity.kt
+++ app/src/main/java/com/louiskirsch/quickdynalist/DetailsActivity.kt
@@ -1,15 +1,28 @@
 package com.louiskirsch.quickdynalist
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import androidx.core.content.FileProvider
 import com.louiskirsch.quickdynalist.objectbox.DynalistItem
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.activity_details.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
 
 class DetailsActivity : AppCompatActivity() {
+
+    private var cachedImage: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +41,55 @@ class DetailsActivity : AppCompatActivity() {
 
         itemImage.visibility = View.GONE
         displayItem.image?.let {
-            Picasso.get().load(it).into(itemImage, object: Callback {
-                override fun onError(e: Exception?) {}
-                override fun onSuccess() {
-                    itemImage.visibility = View.VISIBLE
-                }
-            })
+            Picasso.get().load(it).run {
+                into(itemImage, object: Callback {
+                    override fun onError(e: Exception?) {}
+                    override fun onSuccess() {
+                        itemImage.visibility = View.VISIBLE
+                    }
+                })
+                into(object: Target {
+                    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
+                    override fun onBitmapFailed(e: java.lang.Exception?, errorDrawable: Drawable?) {}
+                    override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                        itemImage.setOnClickListener {
+                            val contentUri = generateContentURI(bitmap!!)
+                            openInGallery(contentUri)
+                        }
+                    }
+                })
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        clearCache()
+    }
+
+    private fun clearCache() {
+        if (cachedImage != null) {
+            cachedImage!!.delete()
+        }
+    }
+
+    private fun generateContentURI(bitmap: Bitmap): Uri {
+        if (cachedImage == null) {
+            val path = File(cacheDir, "image-cache")
+            path.mkdir()
+            cachedImage = File(path, UUID.randomUUID().toString() + ".png")
+            val stream = FileOutputStream(cachedImage)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        }
+        return FileProvider.getUriForFile(this,
+                "com.louiskirsch.quickdynalist.fileprovider", cachedImage!!)
+    }
+
+    private fun openInGallery(contentUri: Uri) {
+        Intent().apply {
+            action = Intent.ACTION_VIEW
+            setDataAndType(contentUri, "image/*")
+            startActivity(this)
         }
     }
 
