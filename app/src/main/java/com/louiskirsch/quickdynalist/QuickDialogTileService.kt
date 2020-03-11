7324a335dbab8b7704349e82bf27d1c67950d743
Louis Kirsch <louis@louiskirsch.com>
2020-03-11 21:02:31 +0100

    Added quick toggle for Quick Dialog

---
+++ app/src/main/java/com/louiskirsch/quickdynalist/QuickDialogTileService.kt
@@ -0,0 +1,24 @@
+package com.louiskirsch.quickdynalist
+
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+class QuickDialogTileService : TileService() {
+
+    private fun showQuickDialog() {
+        startActivityAndCollapse(Intent(this, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        })
+    }
+
+    override fun onClick() {
+        if (isLocked) {
+            unlockAndRun { showQuickDialog() }
+        } else {
+            showQuickDialog()
+        }
+    }
+}
